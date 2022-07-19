daba011731abebab7a67a3fed409561c3816e617
ngusya <ngusyakelvin@gmail.com>
2022-07-19 13:12:16 +0300

    Fixed Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash                                                                                
 # Will set up a web server ready for deployment of webstatic                                       
-sudo apt-get update                                                                                
-sudo apt-get install -y nginx                                                                      
+ apt-get update                                                                                
+ apt-get install -y nginx                                                                      
                                                                                                    
-sudo ufw allow 'Nginx HTTP'                                                                        
+ ufw allow 'Nginx HTTP'                                                                        
                                                                                                    
 # Creates /data/web_static/releases/test/ and /data/web_static/shared/ folders                     
-sudo mkdir -p /data/                                                                               
-sudo mkdir -p /data/web_static/                                                                    
-sudo mkdir -p /data/web_static/releases/test/                                                      
-sudo mkdir -p /data/web_static/shared/                                                             
+ mkdir -p /data/                                                                               
+ mkdir -p /data/web_static/                                                                    
+ mkdir -p /data/web_static/releases/test/                                                      
+ mkdir -p /data/web_static/shared/                                                             
 #Creates a sample html file                                                                        
-sudo echo "<html>                                                                                  
+ echo "<html>                                                                                  
             <head>                                                                                     
             </head>                                                                                    
             <body>                                                                                     
@@ -24,19 +24,18 @@ then
     rm /data/web_static/current                                                                    
 fi                                                                                                 
                                                                                                    
-#Creates a symbolic link /data/web_static/current linked to /data/web_static/releases/test/ folder 
-sudo ln -sf /data/web_static/releases/test /data/web_static/current                                
+#Creates a symbolic link /data/web_static/current linked to /data/web_static/releases/test/ folder
+ln -sf /data/web_static/releases/test /data/web_static/current                                
                                                                                                    
-#Gives ownership of the /data/ folder to the ubuntu user AND group                                 
-sudo chown -R ubuntu:ubuntu /data/                                                                 
-sudo chgrp -R ubuntu /data/                                                                        
+#Gives ownership of the /data/ folder to the ubuntu user AND group
+chown -R ubuntu:ubuntu /data/
+chgrp -R ubuntu /data/                                                                        
                                                                                                    
 #Updates the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static  
-sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}
                                                                                                    
 #Restarts Nginx                                                                                    
 service nginx restart
 
 #Ensures program exits successfully
 exit 0
-
