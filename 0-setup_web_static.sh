8272487209255b1163c80570e98876b2e28483e4
ngusya <ngusyakelvin@gmail.com>
2022-07-19 12:57:25 +0300

    Fixed Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,12 +12,12 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/                                                             
 #Creates a sample html file                                                                        
 sudo echo "<html>                                                                                  
-        <head>                                                                                     
-        </head>                                                                                    
-        <body>                                                                                     
-          Holberton School                                                                         
-        </body>                                                                                    
-      <html>" > /data/web_static/releases/test/index.html                                          
+            <head>                                                                                     
+            </head>                                                                                    
+            <body>                                                                                     
+              Holberton School                                                                         
+            </body>                                                                                    
+           </html>" > /data/web_static/releases/test/index.html                                          
                                                                                                    
 if [[ -L /data/web_static/current ]]                                                               
 then                                                                                               
