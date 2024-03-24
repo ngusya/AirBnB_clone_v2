18abb132a6e664138fe12b66eaf8582b07da0c5b
Carthage <carthage@ubuntu.myguest.virtualbox.org>
2024-03-24 15:58:06 +0300

    Fixed task 7

--- web_flask/models/state.py
+++ web_flask/models/state.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """Defines the State class."""
 import models
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
-from models.city import City
 from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
@@ -20,20 +22,14 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="all, delete")
-    else:
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """Get a list of City instances with
-                state_id equals to the current State.id.
-
-            This is a getter attribute for FileStorage
-                relationship between State and City.
-            """
+            """Get a list of all related City objects."""
             city_list = []
-            for city in models.storage.all(City).values():
+            for city in list(models.storage.all(City).values()):
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
